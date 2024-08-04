5c3e5268d061ae22ea6afed9241ebe3feb4ea796
James Harkins <jh.harkins@protonmail.com>
2024-08-04 15:40:33 -0400

    feat: add nicknames to golfers and dashboard

--- spec/models/golfer_spec.rb
+++ spec/models/golfer_spec.rb
@@ -13,11 +13,12 @@ RSpec.describe Golfer, type: :model do
   describe 'validations' do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
+    it { should validate_presence_of :nickname }
     it { should validate_presence_of :email }
 
     describe 'email' do
       before do
-        golfer_1 = Golfer.create!(first_name: 'Tony', last_name: 'Soprano', email: 'wokeupthismorning@gmail.com', password: 'test123', password_confirmation: 'test123')
+        golfer_1 = Golfer.create!(first_name: 'Tony', last_name: 'Soprano', nickname: 'T', email: 'wokeupthismorning@gmail.com', password: 'test123', password_confirmation: 'test123')
       end
 
       it { should allow_value('cleavermovie@gmail.com').for(:email) }
@@ -28,13 +29,13 @@ RSpec.describe Golfer, type: :model do
     it { should have_secure_password }
 
     it 'should not have a password attribute and the password_digest attribute should be a hash' do
-      golfer = Golfer.create!(first_name: 'Tony', last_name: 'Soprano', email: 'wokeupthismorning@gmail.com', password: 'test123', password_confirmation: 'test123')
+      golfer = Golfer.create!(first_name: 'Tony', last_name: 'Soprano', nickname: 'T', email: 'wokeupthismorning@gmail.com', password: 'test123', password_confirmation: 'test123')
       expect(golfer).to_not have_attribute(:password)
       expect(golfer.password_digest).to_not eq('password123')
     end
 
     it 'should be initialized with a default role' do
-      golfer = Golfer.create!(first_name: 'Tony', last_name: 'Soprano', email: 'wokeupthismorning@gmail.com', password: 'test123', password_confirmation: 'test123')
+      golfer = Golfer.create!(first_name: 'Tony', last_name: 'Soprano', nickname: 'T', email: 'wokeupthismorning@gmail.com', password: 'test123', password_confirmation: 'test123')
       
       expect(golfer.role).to eq('default')
     end
@@ -42,7 +43,7 @@ RSpec.describe Golfer, type: :model do
 
   describe 'instance methods' do
     before do
-      @golfer_1 = Golfer.create!(first_name: 'Tony', last_name: 'Soprano', email: 't@badabing.com', password: 'test123', password_confirmation: 'test123')
+      @golfer_1 = Golfer.create!(first_name: 'Tony', last_name: 'Soprano', nickname: 'T', email: 't@badabing.com', password: 'test123', password_confirmation: 'test123')
 
       @trip_1 = Trip.create!(year: 2013, number: 'XIII', location: 'Dewey Beach')
       @trip_2 = Trip.create!(year: 2014, number: 'XIV', location: 'Rehoboth Beach')
